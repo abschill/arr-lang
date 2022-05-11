58709b83163b5694e4aedc6bb5fc3c4bbe4fdee4
abschill <ajay198810@gmail.com>
2022-05-11 12:25:56 -0600

    add: line num+pos

--- src/arrlang.c
+++ src/arrlang.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+void handle_inline(char c, int pos, int ln) {
+    // print char, line&position 
+    // todo: actually parse lol
+    printf("Character: %c\n", c);
+    printf("Line: %d & Position: %d\n", ln, pos);
+}
+
 int main(int argc, char*argv[]) {
+    int file_pos = 0;
+    int line_no = 0;
     char* input_file;
     char c;
+
+    // if no input arg then just run the test file for now
     if(argc == 1) {
         input_file = "example0.arr";
     }
@@ -15,15 +26,21 @@ int main(int argc, char*argv[]) {
     // get file by each char as a stream
     c = fgetc(content);
     while(c != EOF) {
+        // inc the pos for every char in the stream
+        file_pos++;
         if(c == '\n') {
+            // if its a line break inc the line no
+            line_no++;
             printf("\n");   
         }
         else {
-            printf("%c", c);
+            handle_inline(c, file_pos, line_no);
         }
+        // get next char in stream
         c = fgetc(content);
     }
     printf("\n");
+    // close stream
     fclose(content);
     return 0;
 }
