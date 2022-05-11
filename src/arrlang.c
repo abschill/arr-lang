c400e05f22bbd2d85782bfa1475f997ea9895e67
abschill <ajay198810@gmail.com>
2022-05-11 11:49:38 -0600

    update: open test file stream

--- src/arrlang.c
+++ src/arrlang.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World\n");
+int main(int argc, char*argv[]) {
+    char* input_file;
+    char c;
+    if(argc == 1) {
+        input_file = "data.arr";
+    }
+    else {
+        if(argc == 2)
+            input_file = argv[1];
+    }
+    FILE* content = fopen(input_file, "r");
+    
+    // get file by each char as a stream
+    c = fgetc(content);
+    while(c != EOF) {
+        if(c == '\n') {
+            printf("\n");   
+        }
+        else {
+            printf("%c", c);
+        }
+        c = fgetc(content);
+    }
+    printf("\n");
+    fclose(content);
     return 0;
 }
