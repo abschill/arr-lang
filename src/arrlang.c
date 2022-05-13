e153ae5a347fd0ecf38b52e3aa70209ff900bdda
abschill <ajay198810@gmail.com>
2022-05-12 20:46:11 -0600

    prettyprint

--- src/arrlang.c
+++ src/arrlang.c
@@ -1,17 +1,38 @@
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
+#include "color.c"
+
 char _cache[] = {};
 
 void append_char(char c, int pos, int ln) {
     char last_two[2] = {_cache[pos-1], c};
-    // print char, line&position 
+    // print char, line&position
     // todo: actually parse lol
     strcat(_cache, &c);
-    printf("Character: %c\n", c);
-    printf("Line: %d & Position: %d\n", ln, pos);
+    purple();
+    printf("Character: ");
+    reset();
+    printf("%c\n", c);
+    yellow();
+    printf("Line: ");
+    reset();
+    printf("%d\n", ln);
+    yellow();
+    printf("Position: ");
+    reset();
+    printf("%d\n", pos);
     // -1 pos for last char + this char
-    printf("Last 2: %s\n", last_two);
+    printf("Last 2: %s\n\n", last_two);
+}
+
+void print_cache() {
+    printf("\n");
+    yellow();
+    printf("input file:\n");
+    reset();
+    printf("%s", _cache);
+    printf("\n");
 }
 
 int main(int argc, char*argv[]) {
@@ -29,27 +50,21 @@ int main(int argc, char*argv[]) {
             input_file = argv[1];
     }
     FILE* content = fopen(input_file, "r");
-    
     // get file by each char as a stream
     c = fgetc(content);
     while(c != EOF) {
         // inc the pos for every char in the stream
         file_pos++;
+        append_char(c, file_pos, line_no);
         if(c == '\n') {
             // if its a line break inc the line no
             line_no++;
-            printf("\n");   
-        }
-        else {
-            
-            append_char(c, file_pos, line_no);
+            printf("\n");
         }
         // get next char in stream
         c = fgetc(content);
     }
-    printf("\n");
-    printf("The File:\n");
-    printf("%s", _cache);
+    print_cache();
     // close stream
     fclose(content);
     return 0;
